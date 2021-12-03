d3de05645a6154424ffa0eb4364044713b3b1826
Julieta Cozza <julietacozza@Julietas-MBP.fibertel.com.ar>
2021-12-03 11:54:23 -0300

    cart order modal

--- src/components/Cart/CheckOut.jsx
+++ src/components/Cart/CheckOut.jsx
@@ -13,18 +13,16 @@ const CheckOut = (props) => {
 
     const generateOrder = (e) => {
         e.preventDefault()
-        
+
         const order = {}
-        
-        // order.date = firebase.firestore.Timestamp.fromDate(new Date());
+
         order.buyer = { name, email, phone }
-        order.price = totalPrice;
-
+        order.price = totalPrice();
         order.items = cartList.map((item) => {
-            const id = item.prod.id
-            const title = item.prod.name
+            const id = item.id
+            const title = item.name
 
-            return { id, name }
+            return { id, title }
         })
 
         //añado la orden a db
@@ -32,49 +30,81 @@ const CheckOut = (props) => {
         const orderCollection = db.collection('orders');
 
         orderCollection.add(order)
-            .then((IdDocument) => { 
-                setIdOrder(IdDocument.id) 
+            .then((IdDocument) => {
+                setIdOrder(IdDocument.id)
             })
             .catch(err => console.log(err))
     }
 
-    
-
     return (
         <div>
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Ingreso de datos
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <form onSubmit={generateOrder}>
-                        <label type="text" name='name' > nombre</label>
-                        <input type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)} />
-                        <label name='email'> email</label>
-                        <input type="text"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)} />
-                        <label name='phone'> telefono de contacto</label>
-                        <input type="text" name="phone" id=""
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)} />
-                            <button> enviar orden </button>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-               <Button onClick={clearCart}> cerrar </Button>
-                </Modal.Footer>
-                <h1>{idOrder ? `su codigo de orden es: ${idOrder}` : null} </h1>
-            </Modal>
+            {
+                idOrder ?
+                    <Modal
+                        {...props}
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered>
+                        <Modal.Header closeButton onClick={clearCart}>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Su orden
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <h3>
+                                Gracias por su compra {name} <br/> <br/>
+                                Su código de órden es: {idOrder}
+                            </h3>
+                            
+                        </Modal.Body>
+                        <Modal.Footer>
+                        </Modal.Footer>
+                    </Modal>
+
+                    :
+
+                    <Modal 
+                        {...props}
+                        size="md"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Ingreso de datos
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <form onSubmit={generateOrder} style={{"margin":'30px'}}>
+                                <label type="text" name='name' style={{"marginRight":'10px'}}> Nombre</label >
+                                <input type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)} 
+                                    style={{'width':'280px'}}
+                                    placeholder=' ingrese su nombre'/> <br /> 
+                                <label name='email' style={{"marginRight":'10px'}}> Email</label>
+                                <input type="text"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    style={{'width':'299px'}} 
+                                    placeholder=' ingrese su email'/> 
+                                    <br />
+                                <label name='phone' style={{"marginRight":'10px'}}> Teléfono</label>
+                                <input type="text" name="phone" id=""
+                                    value={phone}
+                                    style={{'width':'276px'}}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                    placeholder=' ingrese su teléfono'/> 
+                                <div style={{"text-align":'center', 'marginTop':'8%'}}>
+                                     <button className="btn btn-outline-secondary ms-5"> enviar orden </button>
+                                </div> 
+                            </form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        </Modal.Footer>
+
+                    </Modal>
+            }
         </div>
     )
 }
